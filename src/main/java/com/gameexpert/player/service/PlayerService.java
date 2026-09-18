3baa1dc684477f300bfbbff68efd5d21c0bc3751
gwangmin-kim <gwangmin@snu.ac.kr>
2026-09-18 11:06:39 +0900

    feat: 레벨 3 - 플레이어 등록

--- src/main/java/com/gameexpert/player/service/PlayerService.java
+++ src/main/java/com/gameexpert/player/service/PlayerService.java
@@ -14,13 +14,16 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
-
     private final PlayerRepository playerRepository;
 
     @Transactional
     public void createPlayer(CreatePlayerRequest request) {
-        // TODO Lv 3: 닉네임 중복을 확인하고 플레이어를 저장합니다.
-        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
+        // Lv 3: 닉네임 중복 확인, 플레이어를 저장
+        String nickname = request.getNickname();
+        if (playerRepository.existsByNickname(nickname)) {
+            throw new ConflictException("DUPLICATE_NICKNAME");
+        }
+        savePlayer(new Player(nickname));
     }
 
     private void savePlayer(Player player) {
