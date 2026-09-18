1f1f7c02ba0bc53680d961fcb076c610df0663b8
gwangmin-kim <gwangmin@snu.ac.kr>
2026-09-18 11:06:25 +0900

    feat: 레벨 3 - 플레이어 요청 검증

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,14 +1,17 @@
 package com.gameexpert.player.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CreatePlayerRequest {
-
-    // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    // Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증
+    @NotNull
+    @Size(min = 2, max = 12)
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private final String nickname;
-
-    public CreatePlayerRequest(String nickname) {
-        this.nickname = nickname;
-    }
 }
