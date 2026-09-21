e7ed5949c694cf9fbc249293a0b49d8dfb2ba7e5
gwangmin-kim <gwangmin@snu.ac.kr>
2026-09-21 20:37:09 +0900

    feat: 레벨 13 - 웹소켓 채팅 핸들러, 웹소켓 채팅 응답 DTO

--- src/main/java/com/gameexpert/ws/dto/ChatResponse.java
+++ src/main/java/com/gameexpert/ws/dto/ChatResponse.java
@@ -1,11 +1,29 @@
 package com.gameexpert.ws.dto;
 
 import java.time.LocalDateTime;
+
+import com.gameexpert.chat.dto.ChatMessageResponse;
 import lombok.Getter;
 
 @Getter
 public class ChatResponse {
-    // TODO Lv 13: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
+    // Lv 13: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
+    private final String type = "chat";
+    private final String sender;
+    private final String content;
+    private final LocalDateTime timestamp;
+
     public ChatResponse(String sender, String content, LocalDateTime timestamp) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public static ChatResponse from(ChatMessageResponse chatMessageResponse) {
+        return new ChatResponse(
+            chatMessageResponse.getSender(),
+            chatMessageResponse.getContent(),
+            chatMessageResponse.getCreatedAt()
+        );
     }
 }
